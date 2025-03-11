b4b2efd8b4c621f186be0eae392b44349251a4c0
student <student@STUDENT-5>
2025-03-11 09:55:16 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿class Algorytm
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Wprowadź a");
+        int a=int.Parse(Console.ReadLine());
+        Console.WriteLine("Wprowadź b");
+        int b=int.Parse(Console.ReadLine());
+        if (a != b)
+        {
+            if (a > b)
+            {
+                a /= a - b;
+                Console.WriteLine(a);
+            }
+            else
+            {
+                b /= b - a;
+                Console.WriteLine(b);   
+            }
+        }
+        else
+        {
+            Console.WriteLine(a);
+            
+        }
+    }
+}
